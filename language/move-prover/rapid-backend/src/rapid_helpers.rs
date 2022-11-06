2b2fc088eb877ad9fa98430b73ba4ac3e6831f79
SakshamBajaj <saksham421@gmail.com>
2022-11-06 14:06:05 -0500

    * rapid_type_suffix helper function added

--- language/move-prover/rapid-backend/src/rapid_helpers.rs
+++ language/move-prover/rapid-backend/src/rapid_helpers.rs
@@ -23,3 +23,22 @@ pub fn rapid_var_declaration(env: &GlobalEnv, ty: &Type, id: &string) -> String{
         }
     }
 }
+
+/// Returns the suffix to specialize a name for the given type instance.
+pub fn rapid_type_suffix(env: &GlobalEnv, ty: &Type) -> String {
+    use PrimitiveType::*;
+    use Type::*;
+    match ty {
+        Primitive(p) => match p {
+            U8 => "u8".to_string(),
+            U64 => "u64".to_string(),
+            U128 => "u128".to_string(),
+            Num => "num".to_string(),
+            Bool => "bool".to_string(),
+            _ => format!("<<unsupported {:?}>>", ty),
+        },
+        Vector(et) => "vec".to_string(),
+        Struct(..) | Fun(..) | Tuple(..) | TypeDomain(..) | ResourceDomain(..) | Error | Var(..)
+        | Reference(..) => format!("<<unsupported {:?}>>", ty),
+    }
+}
